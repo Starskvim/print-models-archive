ec68e0d77490c5dc8e97b35629985b7cd24e1906
Mikhail Gaivola <starskvim@mail.ru>
2023-05-09 13:34:11 +0300

    Initial commit

---
+++ src/main/kotlin/com/starskvim/printmodelsarchive/PrintModelsArchiveApplication.kt
@@ -0,0 +1,11 @@
+package com.starskvim.printmodelsarchive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrintModelsArchiveApplication
+
+fun main(args: Array<String>) {
+	runApplication<PrintModelsArchiveApplication>(*args)
+}
---
+++ src/test/kotlin/com/starskvim/printmodelsarchive/PrintModelsArchiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.starskvim.printmodelsarchive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrintModelsArchiveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
