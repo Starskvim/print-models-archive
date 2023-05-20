3bb83184b67d198f422f5545ce2b9513598a5816
Mikhail Gaivola <starskvim@mail.ru>
2023-05-20 18:46:42 +0300

    Update

--- src/main/kotlin/com/starskvim/printmodelsarchive/domain/CategoriesInfoService.kt
+++ src/main/kotlin/com/starskvim/printmodelsarchive/domain/CategoriesInfoService.kt
@@ -1,7 +1,11 @@
 package com.starskvim.printmodelsarchive.domain
 
 import com.starskvim.printmodelsarchive.persistance.CategoriesInfoDataService
+import com.starskvim.printmodelsarchive.persistance.model.CategoriesInfoData
+import com.starskvim.printmodelsarchive.persistance.model.PrintModelData
 import org.springframework.stereotype.Service
+import java.util.*
+import java.util.concurrent.CopyOnWriteArrayList
 
 @Service
 class CategoriesInfoService(
@@ -9,6 +13,25 @@ class CategoriesInfoService(
     private val dataService: CategoriesInfoDataService
 
 ) {
+
+    suspend fun initializeCategoriesInfo(models: CopyOnWriteArrayList<PrintModelData>): CategoriesInfoData {
+        val modelsCategories = mutableListOf<String>()
+        for (model in models) modelsCategories.addAll(model.categories!!)
+        val uniqCategories = mutableSetOf<String>()
+        uniqCategories.addAll(modelsCategories)
+        val categoriesCount = mutableMapOf<String, Int>()
+        for (category in uniqCategories) categoriesCount[category] = Collections.frequency(uniqCategories, category)
+        val categories = mutableListOf<String>()
+        categories.addAll(uniqCategories)
+        val categoriesInfo = CategoriesInfoData(
+            categories,
+            categoriesCount,
+            null,
+            null,
+            null
+        )
+        return dataService.saveCategoriesInfo(categoriesInfo)
+    }
 
     suspend fun getAllCategories(): List<String> = dataService.getAllCategories()
 }
--- src/main/kotlin/com/starskvim/printmodelsarchive/domain/create/CreatePrintModelService.kt
+++ src/main/kotlin/com/starskvim/printmodelsarchive/domain/create/CreatePrintModelService.kt
@@ -1,6 +1,7 @@
 package com.starskvim.printmodelsarchive.domain.create
 
 import com.starskvim.printmodelsarchive.aop.LoggTime
+import com.starskvim.printmodelsarchive.domain.CategoriesInfoService
 import com.starskvim.printmodelsarchive.domain.MinioService
 import com.starskvim.printmodelsarchive.persistance.PrintModelDataService
 import org.springframework.stereotype.Service
@@ -11,12 +12,13 @@ class CreatePrintModelService(
     private val scanService: FolderScanService,
     private val dataService: PrintModelDataService,
     private val minioService: MinioService,
+    private val categoriesInfoService: CategoriesInfoService
 
     ) {
 
     @LoggTime
     suspend fun initializeArchive() {
         val files = scanService.getFilesFromDisk()
-        InitializeArchiveTask(dataService, minioService, files).execute()
+        InitializeArchiveTask(dataService, minioService, categoriesInfoService, files).execute()
     }
 }
