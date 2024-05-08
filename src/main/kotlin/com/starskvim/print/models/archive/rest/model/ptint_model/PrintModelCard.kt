ef45ce18d35cb7762eb4550499a546229c55fa74
Mikhail Gaivola <starskvim@mail.ru>
2024-05-08 14:34:16 +0300

    update for front

--- src/main/kotlin/com/starskvim/print/models/archive/rest/model/ptint_model/PrintModelCard.kt
+++ src/main/kotlin/com/starskvim/print/models/archive/rest/model/ptint_model/PrintModelCard.kt
@@ -5,9 +5,11 @@ import java.time.LocalDateTime
 data class PrintModelCard(
 
     val id: String,
-    var preview: String?,
-    var images: List<String>?,
     val modelName: String?,
+    var preview: String?,
+    var category: String?,
+    var categories: MutableList<String>?,
+    var images: List<String>?,
     val rate: Int?,
     val nsfw: Boolean?,
     val addedAt: LocalDateTime
